bbfe24876614ba5461f8a828115cd1aeb33f5ed0
ppreethi25 <72737532+ppreethi25@users.noreply.github.com>
2021-06-10 18:49:55 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function() {
+
+    jQuery('.autoplay').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        next: false,
+        prev: false,
+        dots: false,
+      });
+    });
+      
+
+ 
